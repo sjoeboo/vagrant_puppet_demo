5400aa4411f909a5c23c91402880b722cb076b98
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2015-12-04 15:00:59 -0500

    add $things

--- Puppetfile
+++ Puppetfile
@@ -9,3 +9,10 @@ mod "puppetlabs/vcsrepo"
 mod "spotify/puppetexplorer"
 
 mod "theforeman/foreman"
+mod "theforeman/foreman_proxy"
+
+mod "sjoeboo/foreman_extras",
+  :git => "https://github.com/sjoeboo/foreman_extras.git"
+
+mod "sjoeboo/puppet_common",
+  :git => "https://github.com/sjoeboo/puppet-common.git"
--- puppet_code/environments/production/site/profile/manifests/base.pp
+++ puppet_code/environments/production/site/profile/manifests/base.pp
@@ -1,13 +1,19 @@
 class profile::base {
   #All hosts export thier ssh hostkey
-  @@sshkey { $hostname:
-    ensure => present,
-    type => "rsa",
-    key  => $sshrsakey,
+  @@sshkey { $::hostname:
+    ensure       => present,
+    type         => 'rsa',
+    key          => $::sshrsakey,
     host_aliases => [$::hostname,$::fqdn,$::ipaddress],
   }
 
   #All hosts collect the hostkeys
   Sshkey <<| |>>
-  
+
+  @@host { $::hostname:
+    ensure       => present,
+    ip           => $::ipaddress_enp0s8,
+    host_aliases => [$::hostname,$::fqdn,$::ipaddress_enp0s8],
+  }
+  Host <<| |>>
 }
